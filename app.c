072d000170ae003d6f1fe47356647b2c2a1f1d6f
Tifelabs <boluwatifeolusegun018@gmail.com>
2024-07-24 07:52:42 -0300

    first comment on C

---
+++ app.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+;
+int main(){
+    int a; int b; int sum;
+
+    a = 20;
+    b = 40;
+    sum = a + b;
+
+    printf("The sum of %i and %i is %c", a, b, sum);
+
+    return 0;
+
+
+}
