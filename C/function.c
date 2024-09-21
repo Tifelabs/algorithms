300b055d4df82a9ad3ff96d0a89112efe1b25d5a
Tifelabs <boluwatifeolusegun018@gmail.com>
2024-09-21 01:10:28 -0300

    woking with functions

--- C/function.c
+++ C/function.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sayHi(char name[]);
+// Declaring the function
+void aboutUser(const char* name, const char* place);
 
-int main(){
-    char name[10];
-    sayHi("Tife");
+int main() {
+    // Defining our variables
+    const char* name = "Tife";
+    const char* place = "Canada";
+
+    // Calling the user function
+    aboutUser(name, place);
 
     return 0;
 }
 
-void sayHi(char name[]){
-    printf("Hello %s how are doing?",name);
+// Defining the user function
+void aboutUser(const char* name, const char* place) {
+    printf("Hello my name is %s\n", name);
+    printf("I currently reside in %s\n", place);
 }
-
